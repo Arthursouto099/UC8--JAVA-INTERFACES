0423c8597ef2c2d0939caef40535ea3bd9929449
Arthursouto099 <arthurtavaressouto@gmail.com>
2025-02-20 14:06:13 -0300

    up

--- guessingGame/src/main/java/com/mycompany/guessinggame/Game.java
+++ guessingGame/src/main/java/com/mycompany/guessinggame/Game.java
@@ -2,22 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package guessingGame;
+package com.mycompany.guessinggame;
+
 import java.util.Scanner;
+
+
 /**
  *
  * @author ARTHURSANTOSTAVARESS
  */
-public class Player {
-    private String name;
+public class Game {
+    private int numberToGuess;
     
-    public Player() {
-        Scanner mySc = new Scanner(System.in);
-        System.out.println("Digite seu nome: ");
-        this.name = mySc.nextLine();
+    public Game () {
+        this.numberToGuess = (int) (Math.random() * 100) + 1;
+   }
+    
+    public void start() {
+        
     }
     
-    public String getName() {
-        return this.name;
-    }
 }
